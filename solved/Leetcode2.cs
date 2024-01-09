9107d05d92b3a18f167966037e0f7f383dbedbc1
loop <tony.kusic@gmail.com>
2024-01-09 15:43:51 +0100

    improved leetcode 2

--- solved/Leetcode2.cs
+++ solved/Leetcode2.cs
@@ -9,37 +9,37 @@ public class ListNode {
 
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode solutionRoot = null;
-        ListNode previous = null;
-        int carryVal = 0;
-        while (l1 != null || l2 != null) {
-            ListNode current = new ListNode();
-            int value1 = l1 != null ? l1.val : 0;
-            int value2 = l2 != null ? l2.val : 0;
-            if (solutionRoot == null) {
-                solutionRoot = current;
+        ListNode current = new ListNode();
+        ListNode solutionRoot = current;
+        int value1, value2, sum;
+        bool carryVal = false;
+
+        while (true) {
+            value1 = l1 != null ? l1.val : 0;
+            value2 = l2 != null ? l2.val : 0;
+            sum = value1 + value2;
+            if (carryVal) {sum++;}
+            if (sum > 0) {
+                if (sum < 10) {
+                    current.val = sum;
+                    carryVal = false;
+                } else {
+                    current.val = sum - 10;
+                    carryVal = true;
+                }
             }
-            if (previous != null) {
-                previous.next = current;
-            }
-            current.val = 0;
-            int sum = value1 + value2 + carryVal;
-            if (sum > 0 && sum < 10) {
-                current.val = sum;
-                carryVal = 0;
-            }
-            else if (sum > 0 && sum >= 10) {
-                current.val = sum - 10;
-                carryVal = 1;
-            }
+
             l1 = l1 != null ? l1.next : null;
             l2 = l2 != null ? l2.next : null;
-            previous = current;
+            if (l1 == null && l2 == null) {
+                break;
+            }
+            current.next = new ListNode();
+            current = current.next;
         }
-        if(carryVal == 1) {
-            ListNode current = new ListNode();
-            current.val = carryVal;
-            previous.next = current;
+
+        if(carryVal) {
+            current.next = new ListNode(1);
         }
 
         return solutionRoot;
