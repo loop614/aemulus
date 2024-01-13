541e9f5f9b44b0d4a75e25e8ffde5cdb41c32b00
loop <tony.kusic@gmail.com>
2024-01-13 02:56:20 +0100

    added leetcode 28

---
+++ solved/Leetcode28.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        bool same;
+        int pos = -1;
+        if (haystack.length() == needle.length()) {
+            if(haystack.compare(needle) == 0) {return 0;}
+            return -1;
+        }
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+        for(int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            if (haystack[i] == needle[0]) {
+                same = true;
+                for (int j = i; j < i+needle.length();j++) {
+                    if (haystack[j] != needle[j-i]) {
+                        same = false;
+                        break;
+                    }
+                }
+                if (same) {pos = i;break;}
+            }
+        }
+        return pos;
+    }
+};
+
+int main() {
+    Solution *sol = new Solution();
+    int output;
+
+    output = sol->strStr("baaa", "abbb");
+    cout << "2 == ";
+    cout << output << endl;
+
+    output = sol->strStr("abb", "abaaa");
+    cout << "2 == ";
+    cout << output << endl;
+
+    output = sol->strStr("abc", "c");
+    cout << "2 == ";
+    cout << output << endl;
+
+    output = sol->strStr("a", "a");
+    cout << "1 == ";
+    cout << output << endl;
+
+    output = sol->strStr("a", "b");
+    cout << "-1 == ";
+    cout << output << endl;
+
+    output = sol->strStr("leetcode", "tc");
+    cout << "3 == ";
+    cout << output << endl;
+
+    output = sol->strStr("sadbutsad", "sad");
+    cout << "0 == ";
+    cout << output << endl;
+
+    output = sol->strStr("leetcode", "leeto");
+    cout << "-1 == ";
+    cout << output << endl;
+
+    return 0;
+}
