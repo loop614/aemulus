7f74f8756ce4313e58735c8c98d5e7ce04edcef9
loop <tony.kusic@gmail.com>
2024-04-08 17:54:47 +0200

    improved leetcode 24

--- solved/Leetcode24.java
+++ solved/Leetcode24.java
@@ -4,15 +4,15 @@ Medium
 Given a linked list, swap every two adjacent nodes and return its head.
 You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
 */
-public class Leetcode14 {
+public class Leetcode24 {
     public static void main(String... args) {
         Solution24 sol = new Solution24();
-        ListNode res;
-        ListNode example = new ListNode(1);
-        example.next = new ListNode(2);
-        example.next.next = new ListNode(3);
-        example.next.next.next = new ListNode(4);
-        example.next.next.next.next = new ListNode(5);
+        ListNode24 res;
+        ListNode24 example = new ListNode24(1);
+        example.next = new ListNode24(2);
+        example.next.next = new ListNode24(3);
+        example.next.next.next = new ListNode24(4);
+        example.next.next.next.next = new ListNode24(5);
         res = sol.swapPairs(example);
         while (res != null) {
             System.out.print(res.val);
@@ -21,8 +21,8 @@ public class Leetcode14 {
         }
         System.out.print("\n");
 
-        example = new ListNode(1);
-        example.next = new ListNode(2);
+        example = new ListNode24(1);
+        example.next = new ListNode24(2);
         res = sol.swapPairs(example);
         while (res != null) {
             System.out.print(res.val);
@@ -36,32 +36,35 @@ public class Leetcode14 {
 /**
  * Definition for singly-linked list.
  */
-public class ListNode {
+class ListNode24 {
     int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    ListNode24 next;
+    ListNode24() {}
+    ListNode24(int val) { this.val = val; }
+    ListNode24(int val, ListNode24 next) { this.val = val; this.next = next; }
 }
 
 class Solution24 {
-    public ListNode swapPairs(ListNode head) {
+    public ListNode24 swapPairs(ListNode24 head) {
         if (head == null || head.next == null) {
             return head;
         }
 
-        ListNode headNextCopy = head.next;
-        ListNode temp;
-        ListNode headPrev = null;
+        ListNode24 headNextCopy = head.next;
+        ListNode24 temp;
+        ListNode24 headPrev = null;
+
+        temp = head.next;
+        head.next = head.next.next;
+        temp.next = head;
+        headPrev = head;
+        head = head.next;
 
         while ( head != null && head.next != null ) {
             temp = head.next;
             head.next = head.next.next;
             temp.next = head;
-            if (headPrev != null) {
-                headPrev.next = temp;
-            }
-
+            headPrev.next = temp;
             headPrev = head;
             head = head.next;
         }
