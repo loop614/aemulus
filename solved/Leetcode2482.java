55ed6d0af9341adaf3a5c25cc0b607e8b4545812
loop <tony.kusic@gmail.com>
2024-07-01 13:48:42 +0200

    added leetcode 56

--- solved/Leetcode2482.java
+++ solved/Leetcode2482.java
@@ -1,3 +1,4 @@
+
 import java.util.Arrays;
 
 /*
@@ -12,23 +13,25 @@ A 0-indexed m x n difference matrix diff is created with the following procedure
     Let the number of zeros in the jth column be zerosColj.
     diff[i][j] = onesRowi + onesColj - zerosRowi - zerosColj
 Return the difference matrix diff.
-*/
+ */
 public class Leetcode2482 {
+
     public static void main(String[] args) {
         Solution2482 sol = new Solution2482();
         int[][] output;
 
-        output = sol.onesMinusZeros(new int[][]{{0,1,1},{1,0,1},{0,0,1}});
-        System.out.println(sol.printMatrix(output));
-        System.out.println(sol.areMatricesEqual(output, new int[][]{{0,0,4},{0,0,4},{-2,-2,2}}));
+        output = sol.onesMinusZeros(new int[][]{{0, 1, 1}, {1, 0, 1}, {0, 0, 1}});
+        sol.printMatrix(output);
+        System.out.println(sol.areMatricesEqual(output, new int[][]{{0, 0, 4}, {0, 0, 4}, {-2, -2, 2}}));
 
-        output = sol.onesMinusZeros(new int[][]{{1,1,1},{1,1,1}});
-        System.out.println(sol.printMatrix(output));
-        System.out.println(sol.areMatricesEqual(output, new int[][]{{5,5,5},{5,5,5}}));
+        output = sol.onesMinusZeros(new int[][]{{1, 1, 1}, {1, 1, 1}});
+        sol.printMatrix(output);
+        System.out.println(sol.areMatricesEqual(output, new int[][]{{5, 5, 5}, {5, 5, 5}}));
     }
 }
 
 class Solution2482 {
+
     public int[][] onesMinusZeros(int[][] nums) {
         int[][] res = new int[nums.length][nums[0].length];
         int[] zeroRows = new int[nums.length];
@@ -36,8 +39,8 @@ class Solution2482 {
         int[] zeroCols = new int[nums[0].length];
         int[] oneCols = new int[nums[0].length];
 
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = 0; j < nums[i].length; j++) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
                 if (nums[i][j] == 0) {
                     zeroRows[i]++;
                     zeroCols[j]++;
@@ -49,9 +52,9 @@ class Solution2482 {
             }
         }
 
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = 0; j < nums[i].length; j++) {
-                res[i][j] = oneRows[i] + oneCols[j] - zeroRows[i] - zeroCols[j] ;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                res[i][j] = oneRows[i] + oneCols[j] - zeroRows[i] - zeroCols[j];
             }
         }
 
@@ -59,13 +62,13 @@ class Solution2482 {
     }
 
     public boolean areMatricesEqual(int[][] a, int[][] b) {
-        if(a.length != b.length || a[0].length != b[0].length){
+        if (a.length != b.length || a[0].length != b[0].length) {
             return false;
         }
 
-        for(int i = 0; i < a.length; i++){
-            for(int j = 0; j < a[i].length; j++){
-                if(a[i][j] != b[i][j]){
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] != b[i][j]) {
                     return false;
                 }
             }
@@ -74,11 +77,9 @@ class Solution2482 {
         return true;
     }
 
-    public boolean printMatrix(int[][] a) {
-        for(int i = 0; i < a.length; i++){
+    public void printMatrix(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
             System.out.println(Arrays.toString(a[i]));
         }
-
-        return true;
     }
 }
---
+++ solved/Leetcode56.java
@@ -0,0 +1,82 @@
+/*
+56. Merge Intervals
+Medium
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+and return an array of the non-overlapping intervals that cover all the intervals in the input.
+*/
+import java.util.Arrays;
+
+public class Leetcode56 {
+    public static void main(String[] args) {
+        Solution56 sol = new Solution56();
+        int[][] res;
+
+        res = sol.merge(new int[][]{{1,4},{2,3}});
+        printMatrix(res);
+        System.err.println(areMatricesEqual(res, new int[][] {{1,4}}));
+
+        res = sol.merge(new int[][]{{1,3},{2,6},{8,10},{15,18}});
+        printMatrix(res);
+        System.err.println(areMatricesEqual(res, new int[][] {{1,6},{8,10},{15,18}}));
+
+        res = sol.merge(new int[][] {{1,4},{4,5}});
+        printMatrix(res);
+        System.err.println(areMatricesEqual(res, new int[][] {{1,5}}));
+
+        res = sol.merge(new int[][] {{1,4}, {0,4}});
+        printMatrix(res);
+        System.err.println(areMatricesEqual(res, new int[][] {{0,4}}));
+    }
+
+    private static void printMatrix(int[][] a) {
+        for (int[] a1 : a) {
+            System.out.print(Arrays.toString(a1));
+            System.out.print(" ");
+        }
+        System.out.print("\n");
+    }
+
+    private static boolean areMatricesEqual(int[][] a, int[][] b) {
+        if (a.length != b.length || a[0].length != b[0].length) {
+            return false;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] != b[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
+class Solution56 {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,
+            (int[] a, int[] b) -> {
+                return Integer.compare(a[0], b[0]);
+            }
+        );
+        int[][] res = new int[intervals.length][2];
+        int pivotInt = 0;
+        int pivotRes = 0;
+        while (pivotInt < intervals.length) {
+            int[] curr = new int[2];
+            curr[0] = intervals[pivotInt][0];
+            curr[1] = intervals[pivotInt][1];
+            while (pivotInt + 1 < intervals.length && intervals[pivotInt+1][0] <= curr[1]) {
+                pivotInt++;
+                if (curr[1] < intervals[pivotInt][1]) {
+                    curr[1] = intervals[pivotInt][1];
+                }
+            }
+            res[pivotRes++] = curr;
+            pivotInt++;
+        }
+
+        return Arrays.copyOfRange(res, 0, pivotRes);
+    }
+}
